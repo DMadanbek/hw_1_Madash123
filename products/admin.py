c8ebab4577c01a72e106f38e8d9635319cb81802
DMadanbek <zonguckaevo@gmail.com>
2021-07-06 15:16:47 +0300

    Initial commit

---
+++ products/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from products.models import Category
+from products.models import Reciew
+from products.models import Products
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Reciew)
+admin.site.register(Products)
